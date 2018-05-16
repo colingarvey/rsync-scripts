b7d9abb9bef5a55ecc1b9e4b4e46dcc6d2aa0445
Colin Garvey <colin.garvey@gmail.com>
2018-05-15 17:58:24 -0700

    modified to mount SMB volume

--- rsync-exthdd-to-nas.sh
+++ rsync-exthdd-to-nas.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
 set -eux
+mountpoint=/Volumes/Photos
+exthddDir=/Volumes/cg_exthd_hfs/Photos/2018
+nasDir=/Volumes/Photos/2018
 
-exthddDir=/Volumes/cg_exthd_hfs/Photos
-# Nas variables
-nasUser=garveyc1
-nasHost=garveynas.local
-nasDir=/Volumes/Photos-2
-
-# nasConnect=$nasUser@$nasHost:$nasDir  Commented out to avoid ssh overhead
 SRC=$exthddDir
 TGT=$nasDir
 
-if [ $HOSTNAME !=  "garveyc1-ZSH03M" ]; then
+if [ $(uname) !=  "Darwin" ]; then
 	(>&2 echo "Running from wrong computer, please run on Mac")
 	exit 1
 fi
+# Create dir and mount it 
+if [ ! -d ${mountpoint} ]; then 
+	sudo mkdir -p ${mountpoint} && sudo chown garveyc1 ${mountpoint}
+	mount_smbfs //garveyc1@garveynas.local/Photos ${mountpoint}
+fi
+
+
 
 rsync -avvu  \
-	--no-whole-file \
-	--inplace \
+	--ignore-existing \
 	--itemize-changes \
 	--progress \
 	"${SRC}/" "$TGT"
